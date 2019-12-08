044f258b7a6906a185d836ea792f2d55e1ee6749
DMotsnyi <danyamotsniy@gmail.com>
2019-12-08 22:13:58 +0200

    updt2

--- tester.cpp
+++ tester.cpp
@@ -11,47 +11,65 @@ const int N = 100;
 
 int main ()
 {
-	FILE *T;
+	vector<string> answers;
+	{
+		FILE *T;
+		char str[N];
+		char *estr;
+		
+			T = fopen("text.txt", "r");
+		
+			if (T == NULL) {printf ("Error\n"); return -1;}
+		
+			while (1)
+			   {
+				  estr = fgets(str,sizeof(str),T);
+		
+			      if (estr == NULL)
+			      {
+			            break;
+			      }
+					puts(str);
+					
+					if(strstr(str, "+")){
+						answers.push_back(str);
+					}
+					
+					for(int i =0; i < N; i++){
+		   				str[i] = 0;
+					}
+			   }
+		
+			if ( fclose (T) == EOF) printf ("Error \n");
+		   
+			for (int i = 0; i < N; i++) {
+		        std::cout << answers[i] << std::endl; 
+			}
+			
+			fclose(T);
+	}
+	
+	FILE *T1;
+	char *estr1;
 	char str[N];
-	std::vector<std::string> answers;
-	char *estr;
-   
-	T = fopen("text.txt", "r");
+	
+		T1 = fopen("text.txt", "r");
+	
+		if (T1 == NULL) {printf ("Error\n"); return -1;}
+	
+		while (1)
+		   {
+			  estr1 = fgets(str,sizeof(str),T1);
+	
+		      if (estr1 == NULL)
+		      {
+		            break;
+		      }
+				puts(str);
+		   }
+	
+		if ( fclose (T1) == EOF) printf ("Error \n");
 
-	if (T == NULL) {printf ("Error\n"); return -1;}
-
-	   while (1)
-	   {
-	      estr = fgets(str,sizeof(str),T);
-
-	      if (estr == NULL)
-	      {
-	         if ( feof (T) != 0)
-	         {  
-	            break;
-	         }
-	         else
-	         {
-	            break;
-	         }
-	      }
-			puts(str);
-			
-			int flag = strchr(str, '+') - str ;
-			if(flag != -7404896){
-				answers.push_back(str);
-			}
-			
-			for(int i =0; i < N; i++){
-   				str[i] = 0;
-			}
-	   }
-
-   if ( fclose (T) == EOF) printf ("Error \n");
-   
-   for (int i = 0; i < N; i++) {
-        std::cout << answers[i] << std::endl; 
-    }
 
 
    return 0;
