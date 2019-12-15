28e61eeaad6c440e8a0b0743b3cd011247d725c6
DMotsnyi <danyamotsniy@gmail.com>
2019-12-15 13:40:59 +0200

    newconsole

--- tester.cpp
+++ tester.cpp
@@ -1,6 +1,8 @@
 #include <stdio.h>  
 #include <stdlib.h>
 #include <locale.h>
+#include <windows.h>
+#include <time.h>
 #include <iostream> 
 #include <cstring> 
 #include <vector>
@@ -9,35 +11,37 @@
 using namespace std;
 const int N = 1000;
 
+int getIndexOfChar(char*, char); // finding number of '+'
+
 int main ()
 {
+	SetTimer(NULL,1,1000,NULL);
 	setlocale(LC_ALL, "Rus");
+	srand(time(NULL));
 	
 	vector<char> answers; //array with answers
 	vector<string> all;
 	vector<char> input;
-	{	// block that allow to find answers
+	
+	{
 		FILE *T;
 		char str[N];
 		char *estr, tmp;
 		int i, j, result = 0;
 		
 			T = fopen("text.txt", "r");
-			if (T == NULL) {printf ("Stroke has not found\n"); return -1;}
+			if (T == NULL) {printf ("Строка не найдена\n"); return -1;}
 		
 			for(i = 0; 1; i++)
 			   {
 				  estr = fgets(str,sizeof(str),T);
 			      if (estr == NULL)
 			      {
-			      	puts("Error");
 					break;
 			      }
-
-					int flag = strchr(str, '+') - str ; // finding number of '+'
-					
+				
 					if(strstr(str, "+")){
-						for(i = flag; i < N; ++i){
+						for(i = getIndexOfChar(str, '+'); i < N; ++i){
 							str[i] = 0;
 						}
 						str[strlen(str)] = '\n';	// adding \n after right answer
@@ -51,22 +55,25 @@ int main ()
 			   
 			if ( fclose (T) == EOF) printf ("Error \n");
 
-			puts("\nBegining of the test\n");
+			puts("\nНачало теста\n");
 			
 				for(j = 0; j < all.size()/4; ++j){
+					
 					for (i = 0; i < 4; ++i) {
 						if (i % 4) {
 							cout << all[i + j*4] << endl;
 						} else {
-							cout << "Question: " << all[i + j*4] << endl;
+							cout << "Вопрос: " << all[i + j*4] << endl;
 						}
 					}
 					cin >> tmp;	//fixed
 					cout << endl;
 					input.push_back(tmp);
+					system("pause");
+					system("cls");
 				}
 	
-			puts("\nEnd of the test\n");
+			puts("\nКонец теста\n");
 			
 			for (i = 0; i < answers.size(); ++i) {
 				if(answers[i] == input[i]){
@@ -75,23 +82,23 @@ int main ()
 			}
 			
 			result = (result*100)/answers.size();
-			cout << "Your result: " << result << "%" << endl << endl;
+			cout << "Ваш результат: " << result << "%" << endl << endl;
 			
 			if(result > 90 && result <= 100){
-				printf("Nice");
+				printf("Отлично");
 			} else if(result > 70 && result <= 90){
-				printf("Well");
+				printf("Хорошо");
 			} else if(result > 50 && result <= 70){
-				printf("Good");
+				printf("Удовлетворительно");
 			} else {
-				printf("Bad");
+				printf("Неудовлетворительно");
 			}
 			
 	}
-
-
-
-
-   return 0;
+    return 0;
 } 
+
+int getIndexOfChar(char* str, char symbol){
+   	return strchr(str, symbol) - str;
+}
 
