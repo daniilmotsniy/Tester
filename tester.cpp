273bcc324b2e6a5028643bdbcef5b05a35b91e4a
DMotsnyi <danyamotsniy@gmail.com>
2019-12-15 16:16:56 +0200

    some edits

--- tester.cpp
+++ tester.cpp
@@ -1,7 +1,6 @@
 #include <stdio.h>  
 #include <stdlib.h>
 #include <locale.h>
-#include <windows.h>
 #include <time.h>
 #include <iostream> 
 #include <cstring> 
@@ -9,36 +8,43 @@
 #include <string>
 
 using namespace std;
-const int N = 1000;
+const int N = 512;
 
-int getIndexOfChar(char*, char); // finding number of '+'
+int getIndexOfChar(char*, char); // used in finding number of '+' position
+void printResult(int);
+int getResult(vector<char> input, vector<char> answers);
+//char* selectTest(char* test_path[]);
 
 int main ()
 {
-	SetTimer(NULL,1,1000,NULL);
 	setlocale(LC_ALL, "Rus");
 	srand(time(NULL));
 	
-	vector<char> answers; //array with answers
-	vector<string> all;
+	char test_path[N/4];
+	
+	vector<string> all; //arrays
+	vector<char> answers;
 	vector<char> input;
 	
-	{
-		FILE *T;
+	int i, j; //variables
+	char tmp;
+	
+		FILE *T; //File
 		char str[N];
-		char *estr, tmp;
-		int i, j, result = 0;
+		char *estr;
 		
-			T = fopen("text.txt", "r");
-			if (T == NULL) {printf ("Строка не найдена\n"); return -1;}
+		puts("\nНачало теста, введите путь к документу с тестом\n");
+		gets(test_path);
+
+		T = fopen(test_path, "r");
+		puts("\n");
 		
-			for(i = 0; 1; i++)
-			   {
-				  estr = fgets(str,sizeof(str),T);
-			      if (estr == NULL)
-			      {
-					break;
-			      }
+			if (T == NULL) {printf ("Строка не найдена\n");}
+			for(i = 0; 1; ++i){
+				estr = fgets(str,sizeof(str),T);
+			      	if (estr == NULL){
+						break;
+			      	}
 				
 					if(strstr(str, "+")){
 						for(i = getIndexOfChar(str, '+'); i < N; ++i){
@@ -48,22 +54,20 @@ int main ()
 						answers.push_back(str[0]);
 					}
 					
-					if (str[0] != '\n' && str[1] != 0) {
+					if (str[0] != '\n' && str[1] != 0){
 						all.push_back(str);
 					}
 			   }
 			   
-			if ( fclose (T) == EOF) printf ("Error \n");
-
-			puts("\nНачало теста\n");
-			
+		if ( fclose (T) == EOF) printf ("Error \n");
+  	
 				for(j = 0; j < all.size()/4; ++j){
 					
 					for (i = 0; i < 4; ++i) {
 						if (i % 4) {
 							cout << all[i + j*4] << endl;
 						} else {
-							cout << "Вопрос: " << all[i + j*4] << endl;
+						cout << "Вопрос: " << all[i + j*4] << endl;
 						}
 					}
 					cin >> tmp;	//fixed
@@ -75,14 +79,27 @@ int main ()
 	
 			puts("\nКонец теста\n");
 			
-			for (i = 0; i < answers.size(); ++i) {
+			printResult(getResult(input, answers));
+			
+    return 0;
+} 
+
+int getIndexOfChar(char* str, char symbol){
+   	return strchr(str, symbol) - str;
+}
+
+int getResult(vector<char> input, vector<char> answers){
+	int i, result = 0;
+	for (i = 0; i < answers.size(); ++i) {
 				if(answers[i] == input[i]){
 					result++;
 				}
 			}
-			
-			result = (result*100)/answers.size();
-			cout << "Ваш результат: " << result << "%" << endl << endl;
+	return result = (result*100)/answers.size();
+}
+
+void printResult(int result){
+	cout << "Ваш результат: " << result << "%" << endl << endl;
 			
 			if(result > 90 && result <= 100){
 				printf("Отлично");
@@ -93,12 +110,9 @@ int main ()
 			} else {
 				printf("Неудовлетворительно");
 			}
-			
-	}
-    return 0;
-} 
-
-int getIndexOfChar(char* str, char symbol){
-   	return strchr(str, symbol) - str;
 }
-
+/*
+char (char test_path[N/4]){
+	char* test_path_0 = strcat("textes/", test_path);
+	return strcat(test_path_0, ".txt");
+} */
