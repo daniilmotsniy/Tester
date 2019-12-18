a440053ce1b570fae484669a4749b8a5f427013f
DMotsnyi <danyamotsniy@gmail.com>
2019-12-18 22:23:03 +0200

    read txt

--- tester_java/src/tester/Main.java
+++ tester_java/src/tester/Main.java
@@ -6,17 +6,44 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.io.FileReader;
+import java.util.Scanner;
+import java.util.Vector;
+
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("login.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("test.fxml"));
         primaryStage.setTitle("Tester");
         primaryStage.setScene(new Scene(root, 600, 600));
         primaryStage.show();
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws  Exception{
+
+      /*  Information txt = new Information();
+        txt.getText("res/tests/test1.txt");
+        txt.printText(); */
+
+            Vector<String> text = null;
+
+            FileReader reader = new FileReader("res/tests/test1.txt");
+
+            Scanner scan = new Scanner(reader);
+
+            while (scan.hasNextLine()) {
+                text.addElement(scan.nextLine());
+            }
+
+            reader.close();
+
+            for(int i = 0; i < text.size(); i++){
+                System.out.println(text.get(i));
+            }
+
+
+
         launch(args);
     }
 }
