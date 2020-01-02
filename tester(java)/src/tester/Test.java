8f3f7c06d0a89c036292f8aeeb596a4ca1b75e06
DMotsnyi <danyamotsniy@gmail.com>
2020-01-02 19:06:22 +0200

    adding in hash map

--- tester(java)/src/tester/Test.java
+++ tester(java)/src/tester/Test.java
@@ -7,13 +7,14 @@ import javafx.scene.control.RadioButton;
 import javafx.scene.control.ToggleGroup;
 
 import java.io.FileReader;
+import java.util.HashMap;
 import java.util.Scanner;
 import java.util.Vector;
 
 public class Test {
 
     Vector<String> text = new Vector();
-    Vector<Integer> answers = new Vector();
+    HashMap<Integer, Integer> answers = new HashMap<>();
 
     int i = 0; //Number of block with questions and answers
 
@@ -40,21 +41,23 @@ public class Test {
         answ_rb_2.setToggleGroup(answers_tgl);
 
         getText("res/tests/test1.txt");
-        printText(text);
+        //printText(text);
         setInformation(i);
 
         btn_next.setOnAction(event -> {
             i++;
                 if(i > text.size()/4 - 1)
-                    i=text.size()/4 - 1;
+                    i = text.size()/4 - 1;
             setInformation(i);
+            addAnswer(i);
         });
 
         btn_prev.setOnAction(event -> {
             i--;
                 if(i<0)
-                    i=0;
+                    i = 0;
             setInformation(i);
+            addAnswer(i);
         });
 
     }
@@ -81,20 +84,18 @@ public class Test {
         answ_rb_0.setText(text.elementAt(1 + i*4));
         answ_rb_1.setText(text.elementAt(2 + i*4));
         answ_rb_2.setText(text.elementAt(3 + i*4));
-        if(answers.size() > 0)
-            System.out.println((answers.elementAt(i-1)));
     }
 
-    void addAnswer(){
-        if(answ_rb_0.isSelected()==true){
-            answers.addElement(1);
-        } else if(answ_rb_1.isSelected()==true){
-            answers.addElement(2);
-        } else if(answ_rb_2.isSelected()==true){
-            answers.addElement(3);
+    void addAnswer(int i) {
+        if (answ_rb_0.isSelected() == true && answ_rb_1.isSelected() == false && answ_rb_2.isSelected() == false) {
+            answers.put(i, 0);
+        } else if (answ_rb_0.isSelected() == false && answ_rb_1.isSelected() == true && answ_rb_2.isSelected() == false) {
+            answers.put(i, 1);
+        } else if (answ_rb_0.isSelected() == false && answ_rb_1.isSelected() == false && answ_rb_2.isSelected() == true) {
+            answers.put(i, 2);
         } else {
             System.out.printf("Error");
         }
+        System.out.println(answers.get(i));
     }
-
 }
