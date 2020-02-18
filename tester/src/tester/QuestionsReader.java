606c228cea4581813186d1233d0e648e21936657
Stas <stasstas1807@gmail.com>
2020-02-19 00:36:11 +0200

    Decryption

--- tester/src/tester/QuestionsReader.java
+++ tester/src/tester/QuestionsReader.java
@@ -11,6 +11,13 @@ import java.util.List;
 public class QuestionsReader {
     private static final byte SALT = -42;
 
+    // tokens must be lower case in the code, but they can be any in files
+    private static final String TOKEN_QUESTION = "q";
+    private static final String TOKEN_VARIANT = "a";
+    private static final String TOKEN_PICTURE = "p";
+
+    private static final List<String> TOKENS = Arrays.asList(TOKEN_QUESTION, TOKEN_VARIANT, TOKEN_PICTURE);
+
     private String[] lines;
     private byte[] answers;
 
@@ -19,6 +26,10 @@ public class QuestionsReader {
     private HashMap<Integer, File> pictures;
 
     public QuestionsReader(String path, int variantsCount) throws IOException {
+        this(path, variantsCount, true);
+    }
+
+    public QuestionsReader(String path, int variantsCount, boolean encryptFile) throws IOException {
         List<String> list = readFile(path, variantsCount);
 
         questionSize = 1 + variantsCount;
@@ -30,25 +41,9 @@ public class QuestionsReader {
 
             shuffleAnswers(variantsCount);
 
-            String key = toEncryptedString(answers);
-
-            FileWriter fw = new FileWriter(path + "_");
-            BufferedWriter bw = new BufferedWriter(fw);
-
-            bw.append(key).write("\n");
-
-            for (int i = 0; i < lines.length; ++i) {
-                bw.append((i & 3) == 0 ? "Q" : "A").append("\n").append(lines[i]).write("\n");
-                File picture = pictures == null ? null : pictures.getOrDefault(i / questionSize, null);
-                if ((i & 3) == 3 && picture != null) {
-                    bw.append("P").append("\n").append(picture.getPath()).write("\n");
-                }
+            if (encryptFile) {
+                this.encryptFile(path);
             }
-
-            bw.flush();
-
-            bw.close();
-            fw.close();
         }
     }
 
@@ -83,59 +78,101 @@ public class QuestionsReader {
         return file == null ? null : ImageIO.read(file);
     }
 
-    private List<String> readFile(String path, int variantsCount) throws IOException {
+    public void encryptFile(String path) throws IOException {
+        String key = toEncryptedString(answers);
+
+        BufferedWriter bw = new BufferedWriter(new FileWriter(path));
+
+        bw.append(key).write('\n');
+
+        for (int i = 0; i < lines.length; ++i) {
+            bw.append((i & 3) == 0 ? TOKEN_QUESTION : TOKEN_VARIANT).append('\n').append(lines[i]).write('\n');
+            File picture = pictures == null ? null : pictures.getOrDefault(i / questionSize, null);
+            if ((i & 3) == 3 && picture != null) {
+                bw.append(TOKEN_PICTURE).append('\n').append(picture.getPath()).write('\n');
+            }
+        }
+
+        bw.flush();
+        bw.close();
+    }
+
+    public void decryptFile(String path, boolean shuffleBack) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new FileWriter(path));
+
+        for (int i = 0; i < getQuestionsCount(); ++i) {
+            int j = i * questionSize;
+            if (answers[i] != 0) {
+                String t = lines[j + answers[i] + 1];
+                lines[j + answers[i] + 1] = lines[j + 1];
+                lines[j + 1] = t;
+            }
+
+            bw.append(TOKEN_QUESTION).append('\n').append(lines[j]).write('\n');
+
+            for (int k = 1; k < questionSize; ++k) {
+                bw.append(TOKEN_VARIANT).append('\n').append(lines[j + k]).write('\n');
+            }
+
+            File picture = pictures == null ? null : pictures.getOrDefault(i, null);
+            if (picture != null) {
+                bw.append(TOKEN_PICTURE).append('\n').append(picture.getPath()).write('\n');
+            }
+        }
+
+        bw.flush();
+        bw.close();
+
+        if (shuffleBack) {
+            shuffleAnswers(questionSize - 1);
+        }
+    }
+
+    private List<String> readFile(String path, int variantsCount) throws IOException { // TODO improve checking
         List<String> result = new ArrayList<>(32);
 
-        FileReader fr = new FileReader(path);
-        BufferedReader br = new BufferedReader(fr);
-        String line;
+        BufferedReader br = new BufferedReader(new FileReader(path));
+        String line, key = null;
         int lineIndex = 1;
-
-        String key = null;
 
         for (; (line = br.readLine()) != null; ++lineIndex) {
             line = line.trim();
 
-            if (line.length() == 0) {
+            if (line.length() == 0) // skip blanks
                 continue;
-            }
 
-            if (line.matches("^[0-9]+$")) {
+            if (line.matches("^[0-9]+$")) { // find key
                 key = line;
 
                 line = br.readLine();
             }
 
-            if (line.equalsIgnoreCase("q")) {
+            if (line.equalsIgnoreCase(TOKEN_QUESTION)) // find first token
                 break;
-            }
 
             br.close();
-            fr.close();
             throw new IOException(String.format("Unexpected line at %d: %s", lineIndex, line));
         }
 
-        Character previousToken = 'q', lastToken = null;
+        String previousToken = TOKEN_QUESTION, lastToken = null;
         int sameTokensInARow = 1;
         int questionIndex = 0;
 
         do {
             for (int j = 0; j < 1 + variantsCount; ++j) {
-                Pair<Integer, Pair<Character, String>> pair = readLinesUntilNextBreakToken(br, lineIndex);
+                Pair<Integer, Pair<String, String>> pair = readLinesUntilNextBreakToken(br, lineIndex);
                 lineIndex = pair.getKey();
                 lastToken = pair.getValue().getKey();
                 line = pair.getValue().getValue();
 
-                if (previousToken == 'p') {
-                    if (pictures == null) {
+                if (previousToken.equals(TOKEN_PICTURE)) {
+                    if (pictures == null)
                         pictures = new HashMap<>(4);
-                    }
 
                     File file = new File(line);
 
                     if (!file.exists()) {
                         br.close();
-                        fr.close();
                         throw new FileNotFoundException(String.format("Image %s not found", line));
                     }
 
@@ -145,18 +182,16 @@ public class QuestionsReader {
                 }
 
                 if (lastToken != null) {
-                    if (lastToken == 'p') {
+                    if (lastToken.equals(TOKEN_PICTURE))
                         --j;
-                    }
 
-                    if (lastToken == previousToken) {
+                    if (lastToken.equals(previousToken)) {
                         ++sameTokensInARow;
 
-                        if (lastToken == 'a' && sameTokensInARow > variantsCount
-                                || (lastToken == 'q' || lastToken == 'p') && sameTokensInARow > 1) {
+                        if (lastToken.equals(TOKEN_VARIANT) && sameTokensInARow > variantsCount
+                                || (lastToken.equals(TOKEN_QUESTION) || lastToken.equals(TOKEN_PICTURE)) && sameTokensInARow > 1) {
                             br.close();
-                            fr.close();
-                            throw new IOException(String.format("Unexpected token at %d: %c", lineIndex, lastToken));
+                            throw new IOException(String.format("Unexpected token at %d: %s", lineIndex, lastToken));
                         }
                     } else {
                         sameTokensInARow = 1;
@@ -168,15 +203,13 @@ public class QuestionsReader {
         } while (lastToken != null);
 
         br.close();
-        fr.close();
 
-//        if (!Objects.equals(previousToken, 'a') || sameTokensInARow != variantsCount) { TODO check
+//        if (!Objects.equals(previousToken, 'a') || sameTokensInARow != variantsCount) { // TODO
 //            throw new IOException("Wrong questions count");
 //        }
 
-        if (key != null) {
+        if (key != null)
             answers = toDecryptedArray(key, result.size() / (1 + variantsCount));
-        }
 
         return result;
     }
@@ -214,21 +247,20 @@ public class QuestionsReader {
         }
     }
 
-    private Pair<Integer, Pair<Character, String>> readLinesUntilNextBreakToken(BufferedReader br, int lineIndex) throws IOException {
+    // Pair<lineIndex, Pair<token, data>>
+    private Pair<Integer, Pair<String, String>> readLinesUntilNextBreakToken(BufferedReader br, int lineIndex) throws IOException {
         StringBuilder builder = new StringBuilder();
         String line;
 
-        List<Character> tokens = Arrays.asList('a', 'q', 'p');
-
         while ((line = br.readLine()) != null) {
-            if (line.length() == 1 && tokens.contains(Character.toLowerCase(line.charAt(0)))) {
+            if (TOKENS.contains(line.toLowerCase())) { // TODO add "line.length() == N" if all tokens have the same size
                 break;
             }
             builder.append(line).append('\n');
             ++lineIndex;
         }
 
-        return new Pair<>(lineIndex, new Pair<>(line == null ? null : Character.toLowerCase(line.charAt(0)), builder.toString().trim()));
+        return new Pair<>(lineIndex, new Pair<>(line == null ? null : line.toLowerCase(), builder.toString().trim()));
     }
 
     private static String toEncryptedString(byte[] data) {
